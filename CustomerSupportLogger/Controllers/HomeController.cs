34c54f1b3f4447abb3e1a81ae5772a211911e9f4
ANANTH4004 <balamuruganananth@gmail.com>
2022-11-02 10:32:13 +0530

    Add project files.

---
+++ CustomerSupportLogger/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Dal;
+
+namespace CustomerSupportLogger.Controllers
+{
+    public class HomeController : Controller
+    {
+        UserOperations user = null;
+        public HomeController()
+        {
+            user = new UserOperations();
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(FormCollection collection)
+        {
+            bool valid = user.ValidateUser(Request["Username"], Request["password"]);
+            if (valid)
+            {
+                return RedirectToAction("Privacy");
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Dal/UserOperations.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dal
+{
+    public class UserOperations
+    {
+        CustomerEntities userContext = null;
+        public UserOperations()
+        {
+            userContext = new CustomerEntities();
+        }
+        public bool ValidateUser(string email, string pass)
+        {
+            bool ans = false;
+            var found = userContext.UserInfoes.FirstOrDefault(x => x.Email == email);
+            if (found != null)
+            {
+                if (found.Password == pass)
+                {
+                    ans = true;
+                }
+            }
+            return ans;
+        }
+    }
+}
